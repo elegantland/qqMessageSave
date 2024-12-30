10cc43d1ddb3c30de6b85afbbba87f57b5c98ca3
elegantland <65951582+elegantland@users.noreply.github.com>
2024-12-30 08:18:06 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
+// 插件加载时触发
+exports.onLoad = () => {
+    console.log("[Message Save] 1.0.1 loaded");
+};
+
 // 创建窗口时触发
 exports.onBrowserWindowCreated = (window) => {
-    // window 为 Electron 的 BrowserWindow 实例
-}
+};
 
 // 用户登录时触发
 exports.onLogin = (uid) => {
-    // uid 为 QQNT 的 字符串 标识
-}
-
-// 插件加载时触发
-exports.onLoad = () => {
-    console.log("Message Save 1.0.0 loaded");
 };
